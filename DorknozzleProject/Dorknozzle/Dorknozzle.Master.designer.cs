b02fda0f369f56580ce320f2f49e099d73e7babc
superyaooo <superyaooo@gmail.com>
2015-10-06 10:25:29 -0500

    Dorknozzle proj. Add cmd patch to use VS Website Admin Tool with Visual Studio 2013

--- DorknozzleProject/Dorknozzle/Dorknozzle.Master.designer.cs
+++ DorknozzleProject/Dorknozzle/Dorknozzle.Master.designer.cs
@@ -40,22 +40,13 @@ namespace Dorknozzle {
         protected global::System.Web.UI.WebControls.Image Image1;
         
         /// <summary>
-        /// dorknozzleSiteMap control.
+        /// loginView control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.SiteMapDataSource dorknozzleSiteMap;
-        
-        /// <summary>
-        /// dorknozzleMenu control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu dorknozzleMenu;
+        protected global::System.Web.UI.WebControls.LoginView loginView;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
